9115012b0bdec821555e3c395b7936ec191d8ce4
shanlishoes <shanlishoes@gmail.com>
2026-07-21 18:38:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -12,70 +12,65 @@ async function startCamera() {
 
         const devices = await ZXing.BrowserCodeReader.listVideoInputDevices();
 
-        if (devices.length == 0) {
-
-            alert("هیچ دوربینی پیدا نشد.");
-
+        if (devices.length === 0) {
+            alert("هیچ دوربینی پیدا نشد");
             return;
-
         }
 
         let cameraId = devices[0].deviceId;
 
-        devices.forEach(function (d) {
+        devices.forEach(device => {
 
-            const name = d.label.toLowerCase();
+            const name = (device.label || "").toLowerCase();
 
             if (
                 name.includes("back") ||
                 name.includes("rear") ||
                 name.includes("environment")
             ) {
-
-                cameraId = d.deviceId;
-
+                cameraId = device.deviceId;
             }
 
         });
 
         codeReader.decodeFromVideoDevice(
-
             cameraId,
-
             "reader",
+            (result) => {
 
-            (result, err) => {
-
-                if (result) {
-
+                if(result){
                     document.getElementById("barcode").value = result.text;
-
                 }
 
             }
-
         );
 
-    } catch (e) {
+    } catch(e){
 
-        alert("خطا در باز کردن دوربین\n\n" + e);
+        alert(e);
 
     }
 
 }
 
-document.getElementById("startBtn").onclick = function () {
+window.onload = function(){
 
-    document.getElementById("loginPage").style.display = "none";
+    document.getElementById("startBtn").addEventListener("click",function(){
 
-    document.getElementById("scanPage").style.display = "block";
+        alert("دکمه کار میکند");
 
-    document.getElementById("userLabel").innerText =
-        document.getElementById("user").value;
+        document.getElementById("loginPage").style.display="none";
 
-    document.getElementById("branchLabel").innerText =
-        document.getElementById("branch").value;
+        document.getElementById("scanPage").style.display="block";
 
-    startCamera();
+        document.getElementById("userLabel").innerText =
+            document.getElementById("user").value;
+
+        document.getElementById("branchLabel").innerText =
+            document.getElementById("branch").value;
+
+        startCamera();
+
+    });
 
 };
